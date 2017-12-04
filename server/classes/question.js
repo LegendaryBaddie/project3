9504b07aa25d1fa521e1ad5f5c503822e8b71a3a
Austin White <arw.austin@gmail.com>
2017-12-04 16:50:17 -0500

    Some question backend

---
+++ server/classes/question.js
@@ -0,0 +1,11 @@
+const Question = class Question {
+    constructor(id, content){
+        this.id = id;
+        this.content = content;
+        this.time = 60000;
+    }
+    updateTime() {
+        this.time -= 500;
+    }
+}
+module.exports.Question = Question;
--- server/sockets.js
+++ server/sockets.js
@@ -1,29 +1,43 @@
 const xxh = require('xxhashjs');
 const msg = require('./classes/message.js');
+const quest = require('./classes/question.js');
 let io;
 
+const sockRef = {};
+
 const rooms = {
+    //each room has a messages object which contains all of the messages for the current question
+    //each room also has a questions object which contains all of the queued questions,
     math: {
-        messages: {}
+        connected: {},
+        messages: {},
+        questions: [],
+        currentQuestion: null
     },
     code: {
-        messages: {}
+        connected: {},
+        messages: {},
+        questions: [],
+        currentQuestion: null
     },
     science: {
-        messages: {}
+        connected: {},
+        messages: {},
+        questions:[],
+        currentQuestion: null
     }
 };
 
 const appendRooms = (room, socket) => {
     switch (room) {
         case 'math':
-            rooms.math[socket.hash] = socket;
+            rooms.math.connected[socket.hash] = socket;
             break;
         case 'code':
-            rooms.code[socket.hash] = socket;
+            rooms.code.connected[socket.hash] = socket;
             break;
         case 'science':
-            rooms.science[socket.hash] = socket;
+            rooms.science.connected[socket.hash] = socket;
             break;
         default :
             console.log(`Error appending attepted: ${room}`);
@@ -33,16 +47,34 @@ const appendRooms = (room, socket) => {
 const leaveRooms = (room, socket) => {
     switch (room) {
         case 'math':
-            delete rooms.math[socket.hash];
+            delete rooms.math.connected[socket.hash];
             break;
         case 'code':
-            delete rooms.code[socket.hash];
+            delete rooms.code.connected[socket.hash];
             break;
         case 'science':
-            delete rooms.science[socket.hash];
+            delete rooms.science.connected[socket.hash];
             break;
         default :
             console.log(`Error deleting attepted: ${room}`);
+            break;
+    }
+}
+
+const addToQueue = (data, socket) =>{
+    // decide what room they are in
+    switch (sockRef[socket.hash]) {
+        case 'math':
+            rooms.math.questions.push(new quest.Question(socket.hash,data));
+            break;
+        case 'code':
+            rooms.code.questions.push(new quest.Question(socket.hash,data));
+            break;
+        case 'science':
+            rooms.code.questions.push(new quest.Question(socket.hash,data));
+            break;
+        default :
+            console.log(`Error in add to queue ${sockRef[socket.hash]}`);
             break;
     }
 }
@@ -56,45 +88,85 @@ const setupSockets = (ioServer) => {
         //switch between message rooms
         socket.on('joinRoom', (room) => {
             //should check to make sure the room is a valid room here
-            if(!socket.room){
-                socket.room = room;
+            if(!sockRef[socket.hash]){
+                sockRef[socket.hash] = room;
                 socket.join(room);
                 appendRooms(room, socket);
             }else{
                 socket.leave(room);
                 leaveRooms(room, socket);
-                socket.room = room;
+                sockRef[socket.hash] = room;
                 socket.join(room);
                 //maybe check for messages in room and add them in
             }
-            console.dir(`${socket.hash} joined ${socket.room}`);
+            console.dir(`${socket.hash} joined ${room}`);
         });
         socket.on('newQuestion', (data)=>{
             //add question to queue
-            addToQueue(data, socket.hash);
-            //save socket hash to the question
+            addToQueue(data, socket);
         });
         socket.on('newMessage', (data) => {
             //create unique id for the message
             const messageID = xxh.h32(`${data.name}${new Date().getTime()}`, 0xBABEFACE).toString(16);
-            switch(socket.room){
+            switch(sockRef[socket.hash]){
                 case 'math':
                     rooms.math.messages[messageID] = new msg.Message(data.name, data.message, messageID);
-                    io.to(socket.room).emit('msgFromServer', rooms.math.messages[messageID].toOBJ());
+                    io.to(sockRef[socket.hash]).emit('msgFromServer', rooms.math.messages[messageID].toOBJ());
                     break;
                 case 'code':
                     rooms.code.messages[messageID] = new msg.Message(data.name, data.message, messageID);
-                    io.to(socket.room).emit('msgFromServer', rooms.code.messages[messageID].toOBJ());
+                    io.to(sockRef[socket.hash]).emit('msgFromServer', rooms.code.messages[messageID].toOBJ());
                     break;
                 case 'science':
                     rooms.science.messages[messageID] = new msg.Message(data.name, data.message, messageID);
-                    io.to(socket.room).emit('msgFromServer', rooms.science.messages[messageID].toOBJ());
+                    io.to(sockRef[socket.hash]).emit('msgFromServer', rooms.science.messages[messageID].toOBJ());
                     break;
                 default:
                     console.log('message error');
             }
         });
     });
+    // set tick rate for clock updates, and question change checking to every half a second
+    setInterval(()=>{
+        //check if we need to update clients with a new message aka; no question is being answered
+        // need to do this for each room,
+       
+        let keys = Object.keys(rooms);
+        
+        for(let i=0; i<keys.length; i++){
+            // no question so nothing needs to be done
+            if(!rooms[keys[i]].questions)
+            {
+                continue;
+            }
+            //no question is active
+            if(!rooms[keys[i]].currentQuestion)
+            {
+                //set a new active question 
+                // remove current question from queue
+                rooms[keys[i]].currentQuestion = rooms[keys[i]].questions.splice(0,1);;
+                
+                // emit new question to clients in this room
+                io.to(keys[i]).emit('newQuestion', rooms[keys[i]].currentQuestion);
+                
+            }else{
+                //increment the internal clock for the question
+                rooms[keys[i]].currentQuestion.updateTime();
+
+                if(rooms[keys[i]].currentQuestion.time === 0){
+                    // questions time has run out, send the owner of the question to the results page
+                    // add the question asker to a unique room, and remove them from the room they are in
+                    const asker = rooms[keys[i]].connected[rooms[keys[i]].currentQuestion.id];
+                    asker.leave(rooms[keys[i]]);
+                    asker.join(`${asker.hash}`);
+                    
+                    // load a different question to everyone in the room that isnt the owner.
+                    // we can do this by just setting current question to null
+                    rooms[keys[i]].currentQuestion = null;
+                }
+            }
+        };
+    },500);
 };
 
 module.exports.setupSockets = setupSockets;
